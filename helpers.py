bf14d45ec1166c8e0778d618540f2d10dbce35b0
Antti Myllykoski <amyllykoski@yahoo.com>
2019-07-25 14:49:52 -0700

    feat: Added 16bit grey image saving.

--- helpers.py
+++ helpers.py
@@ -5,6 +5,7 @@ import os
 import pdb
 import pickle
 import argparse
+import png
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -18,6 +19,15 @@ import numpy as np
 import scipy
 import scipy.misc
 
+def save16bitGreyScaleImage(numpyArray, file):
+    if file is None:
+        return
+
+    with open(file, 'wb') as f:
+        img = np.moveaxis(numpyArray, 0, -1).astype(np.uint16)
+#         img = ((img + 1.0 ) * 256).astype(np.uint16)
+        writer = png.Writer(width=img.shape[1], height=img.shape[0], bitdepth=16, greyscale=True)
+        writer.write(f, img[:,:,0].tolist())
 
 def checkpoint(iteration, G_XtoY, G_YtoX, D_X, D_Y, checkpoint_dir='checkpoints_cyclegan'):
     """Saves the parameters of both generators G_YtoX, G_XtoY and discriminators D_X, D_Y.
@@ -31,6 +41,12 @@ def checkpoint(iteration, G_XtoY, G_YtoX, D_X, D_Y, checkpoint_dir='checkpoints_
     torch.save(D_X.state_dict(), D_X_path)
     torch.save(D_Y.state_dict(), D_Y_path)
 
+def save_images(sources, targets, iteration, sample_dir='generated'):
+    for idx, (s, t) in enumerate(zip(sources, targets)):
+        path = os.path.join(sample_dir, '{:06d}-{}-real.png'.format(iteration, idx))
+        save16bitGreyScaleImage(s, path) 
+        path = os.path.join(sample_dir, '{:06d}-{}-fake.png'.format(iteration, idx))
+        save16bitGreyScaleImage(t, path) 
 
 def merge_images(sources, targets, batch_size=16):
     """Creates a grid consisting of pairs of columns, where the first column in
@@ -70,6 +86,7 @@ def save_samples(iteration, fixed_Y, fixed_X, G_YtoX, G_XtoY, batch_size=16, sam
     X, fake_X = to_data(fixed_X), to_data(fake_X)
     Y, fake_Y = to_data(fixed_Y), to_data(fake_Y)
     
+    save_images(X, fake_Y, iteration)
     merged = merge_images(X, fake_Y, batch_size)
     path = os.path.join(sample_dir, 'sample-{:06d}-X-Y.png'.format(iteration))
     scipy.misc.imsave(path, merged)
